72fd8d0315437ee2032e0ab3a516a0f9e746c67a
Valencia <starrv2011@gmail.com>
2022-10-15 23:05:28 -0400

    updates

--- src/main/java/com/example/restservice/Member.java
+++ src/main/java/com/example/restservice/Member.java
@@ -7,8 +7,6 @@ public class Member {
 	private final String fname;
 	private final String lname;
 	private final int age;
-	private final Member mother;
-	private final Member father;
 	private ArrayList<Member> children=new ArrayList<Member>();
 	private String greeting="";
 	
@@ -16,46 +14,30 @@ public class Member {
 		this.fname=fname;
 		this.lname=lname;
 		this.age=age;
-		this.mother=null;
-		this.father=null;
 	}
 	
-	public Member(String fname, String lname, int age, Member mother, Member father) {
-		this.age=age;
-		this.father=father;
-		this.mother=mother;
+	public Member(String fname, String lname, int age,String greeting) {
 		this.fname=fname;
 		this.lname=lname;
-	}
-	
-	public Member(String fname, String lname, int age, Member mother, Member father, String greeting) {
 		this.age=age;
-		this.father=father;
-		this.mother=mother;
-		this.fname=fname;
-		this.lname=lname;
 		this.greeting=greeting;
 	}
 	
-	public Member(String fname, String lname, int age, Member mother, Member father, ArrayList<Member> children, String greeting) {
+	public Member(String fname, String lname, int age, ArrayList<Member> children) {
 		this.age=age;
-		this.father=father;
-		this.mother=mother;
+		this.children=children;
+		this.fname=fname;
+		this.lname=lname;
+	}
+	
+	public Member(String fname, String lname, int age, ArrayList<Member> children,String greeting) {
+		this.age=age;
 		this.children=children;
 		this.fname=fname;
 		this.lname=lname;
 		this.greeting=greeting;
 	}
 	
-	public Member(String fname, String lname, int age, Member mother, Member father, ArrayList<Member> children) {
-		this.age=age;
-		this.father=father;
-		this.mother=mother;
-		this.children=children;
-		this.fname=fname;
-		this.lname=lname;
-	}
-
 	public String getFname() {
 		return fname;
 	}
@@ -68,20 +50,16 @@ public class Member {
 		return age;
 	}
 
-	public Member getMother() {
-		return mother;
-	}
-
-	public Member getFather() {
-		return father;
-	}
-
 	public ArrayList<Member> getChildren() {
 		return children;
 	}
 	
 	public String getGreeting() {
 		return greeting;
+	}
+	
+	public void setChildren(ArrayList<Member> children) {
+		this.children=children;
 	}
 
 }
--- src/main/java/com/example/restservice/MemberController.java
+++ src/main/java/com/example/restservice/MemberController.java
@@ -9,21 +9,48 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
 	
-	private ArrayList<Member> children=new ArrayList<Member>();
-	Member Kathy=new Member("Kathy","Starr",50);
-	Member Douglass=new Member("Douglass","Green",55);
-	Member Angela=new Member("Angela","Starr",4);
-	Member Pocohontas=new Member("Pocohontas","Starr",14);
+	Member Valencia;
+	Member Kathy;
+	Member Douglass;
+	Member Angela;
+	Member Pocohontas;
+	Member Koolaid;
 	
 	public MemberController() {
-		children.add(Angela);
+		Douglass=new Member("Douglass","Green",55,"My name is Douglass");
+		Kathy=new Member("Kathy","Starr",50,"My name is Kathy");
+		Koolaid=new Member("Koolaid","Walker",48,"My name is Koolaid");
+		Valencia=new Member("Valencia","Walker",36,"My name is Valencia");
+		
+		ArrayList<Member> children=new ArrayList<Member>();
+		children.add(Valencia);
+		
+		Kathy.setChildren(children);
+		Douglass.setChildren(children);
+		
+		children=new ArrayList<Member>();
+		
+		Pocohontas=new Member("Pocohontas","Walker",14,"My name is Pocohontas");
+		Angela=new Member("Angela","Walker",4,"My name is Angela");
+		
 		children.add(Pocohontas);
+		children.add(Angela);
+		
+		Valencia.setChildren(children);
+		Koolaid.setChildren(children);
 	}
 	
 	@GetMapping("/member")
-	public Member member(@RequestParam(value="name",defaultValue="World") String name) {
-		return new Member("Valencia","Starr",36,Kathy,Douglass,children,String.format("Hello %s",name));
-		
+	public Member[] member(@RequestParam(value="name",defaultValue="World") String name) {
+		Member[] members=new Member[7];
+		members[0]=Douglass;
+		members[1]=Kathy;
+		members[2]=Koolaid;
+		members[3]=Valencia;
+		members[4]=Pocohontas;
+		members[5]=Angela;
+		members[6]=new Member("a","person",2,String.format("Hello %s", name));
+		return members;
 	}
 
 }
