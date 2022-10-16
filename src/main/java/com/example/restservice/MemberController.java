8fe6b2835f2da253e638937339e602aa01a4ae2a
Valencia <starrv2011@gmail.com>
2022-10-16 00:09:44 -0400

    updates

--- src/main/java/com/example/restservice/MemberController.java
+++ src/main/java/com/example/restservice/MemberController.java
@@ -2,6 +2,7 @@ package com.example.restservice;
 
 import java.util.ArrayList;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
